f61c5a7eb2bbe8d9fea39ce56a42e4a396ea0ebc
leonwrathMA <77850594+leonwrathMA@users.noreply.github.com>
2021-01-22 17:45:13 +0100

    added project for task 1.1
    added the visual studio 2019 project for my developer assignment task 1.1

---
+++ Developer Assesment 1.1/Developer Assesment 1.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace Developer_Assesment_1._1
+{
+    class Program
+    {
+        //below is an example of a class variable
+        private static int classVariable { get; set; }
+        //below is an example of an instance variable
+        private int instanceVariable { get; set; }
+
+        //lock object for thread safe variables
+        private readonly object lockObj = new object();
+
+        public int getclassVariableMethod() {
+            lock (lockObj)
+            {
+                return classVariable;
+            }
+        }
+
+        public void setclassVariableMethod(int newValue)
+        {
+            lock (lockObj)
+            {
+                classVariable = newValue;
+            }
+        }
+
+        public int getinstanceVariableMethod()
+        {
+            lock (lockObj)
+            {
+                return instanceVariable;
+            }
+        }
+
+        public void setinstanceVariableMethod(int newValue)
+        {
+            lock (lockObj)
+            {
+                instanceVariable = newValue;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
